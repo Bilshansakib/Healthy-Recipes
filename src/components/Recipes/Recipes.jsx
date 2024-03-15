95ab2c2bfcc1e905379dc09a57c8c5a0005a786d
Bilshansakib <burakaghani@gmail.com>
2024-03-16 00:12:40 +0600

    NavBar Added

---
+++ src/components/Recipes/Recipes.jsx
@@ -0,0 +1,13 @@
+import Recipe from "./Recipe";
+
+const Recipes = () => {
+  return (
+    <div>
+      <Recipe></Recipe>
+      <Recipe></Recipe>
+      <Recipe></Recipe>
+    </div>
+  );
+};
+
+export default Recipes;
